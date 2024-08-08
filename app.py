7d8ef0bc4bee6b56b32011c6a62a9a33d6cad760
Meemo <lzhan202@gmail.com>
2024-08-08 22:53:08 +1000

    implement auto refresh token

--- app.py
+++ app.py
@@ -3,44 +3,46 @@ import time
 import re
 from twilio.rest import Client
 from datetime import datetime
+from playwright.sync_api import sync_playwright
 
 # Twilio Configuration
-# Get below values from your Twilio account
-account_sid = 'XXXXXXX'
-auth_token = 'XXXXXXX'
-twilio_phone_number = '+10000000000'
-your_phone_number = '+61000000000'
+account_sid = 'XXXXXXXXXXXXXXXXXX'
+auth_token = 'XXXXXXXXXXXXXXXXXXX'
+twilio_phone_number = '+12089031693'
+your_phone_number = '+61419328769'
 
-# **Required**:
-# Request URL (Request headers Path)
-# Normally you will only need to change this url in order to get the availability of tickets
-url = "https://apps.customlinc.com.au/puffingbillyrailways/BookingCat/Availability/?AT=5F1BXvWrApR6LSFLpf6Rtj&changeDate&localtime=2024-07-29,%2010:30:30:535"
+# Get new token URLs
+def get_availability_urls():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        found_urls = []
 
-# Request Headers
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Accept": "*/*",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "en-GB,en;q=0.9",
-    "Origin": "https://apps.customlinc.com.au",
-    "Referer": "https://apps.customlinc.com.au/puffingbillyrailways/BookingCat/Availability/?changeDate&localtime=2024-07-29,%2010:30:30:535",
-    "X-Requested-With": "XMLHttpRequest",
-}
+        def handle_request(request):
+            if request.method == 'POST' and 'Availability' in request.url and 'AT=' in request.url:
+                print(f"Found URL: {request.url}")
+                post_data = request.post_data
+                if post_data and 'category=LAKE' in post_data:
+                    print(f"Found URL With LAKE: {request.url}")  
+                    found_urls.append(request.url)
 
-# Request cookie
-cookies = {
-    "currentBrandWAFApplicationBookingCat": "PUFFING%20BILLY",
-    "oidToken": "12594595.467180---EKKRegvApR6n5FLpf6Rj"
-}
+        page.on("request", handle_request)
 
-# Request data, newDate is the date you want to check, DD/MM/YYYY format
-# The API will only return the next 5 days of availability
-# If you want more days, you need to run multiple tasks with different dates
-data = {
-    "newDate": "06/08/2024",
-    "direction": "1"
-}
+        page.goto("https://puffingbilly.com.au/buy-tickets/excursion/")
 
+        page.wait_for_timeout(10000)
+
+        browser.close()
+
+        return found_urls
+
+def modify_url(url):
+    # export AT and localtime parameters
+    at_param = re.search(r'AT=([^&]+)', url).group(1)
+    localtime_param = re.search(r'localtime=([^&]+)', url).group(1)
+    # construct new URL
+    new_url = f"https://apps.customlinc.com.au/puffingbillyrailways/BookingCat/Availability/?AT={at_param}&changeDate&localtime={localtime_param}"
+    return new_url
 def send_sms(message):
     client = Client(account_sid, auth_token)
     client.messages.create(
@@ -49,7 +51,28 @@ def send_sms(message):
         to=your_phone_number
     )
 
-def check_availability():
+
+def check_availability(url):
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "en-GB,en;q=0.9",
+        "Origin": "https://apps.customlinc.com.au",
+        "Referer": "https://apps.customlinc.com.au/puffingbillyrailways/BookingCat/Availability/?changeDate&localtime=2024-07-29,%2010:30:30:535",
+        "X-Requested-With": "XMLHttpRequest",
+    }
+
+    cookies = {
+        "currentBrandWAFApplicationBookingCat": "PUFFING%20BILLY",
+        "oidToken": "12594595.467180---EKKRegvApR6n5FLpf6Rj"
+    }
+
+    data = {
+        "newDate": "06/09/2024",
+        "direction": "1"
+    }
+
     response = requests.post(url, headers=headers, cookies=cookies, data=data)
     html_content = response.text
 
@@ -58,7 +81,7 @@ def check_availability():
     if 'logout' in html_content:
         send_sms("Token 过期，需要重新登录。")
         print(f"{current_time} - Token 过期")
-        return
+        return False  # 返回 False 表示 token 过期
     
     matches = list(re.finditer("Limited Seats", html_content))
     
@@ -78,12 +101,21 @@ def check_availability():
                     send_sms(message)
     else:
         print(f"{current_time} - 无票")
-        
-        
-        
 
-# Optional: Loop requests to refresh tickets
+    return True  
+
+
 while True:
-    check_availability()
-    time.sleep(60)  # check every 60 seconds
-
+    # get availability URLs
+    availability_urls = get_availability_urls()
+    if availability_urls:
+        for availability_url in availability_urls:
+            modified_url = modify_url(availability_url)
+            print(f"Modified URL: {modified_url}")
+            if check_availability(modified_url):
+                break
+        else:
+            print("No valid URL found.")
+    else:
+        print("No Availability URL found.")
+    time.sleep(60)  # retry every 60 seconds
